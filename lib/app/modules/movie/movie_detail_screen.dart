2a0af67b3c87109df752a2a41bad979adff15eaf
Victor Daniel <victordaniel@unigranrio.br>
2023-06-20 17:23:12 -0300

    Implementado sistema de classificacao por estrelas

--- lib/app/modules/movie/movie_detail_screen.dart
+++ lib/app/modules/movie/movie_detail_screen.dart
@@ -1,7 +1,5 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:projeto_iggdrazil/app/core/tmdb/tmdb_api.dart';
-import 'package:projeto_iggdrazil/app/core/ui/todo_list_icons.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class MovieDetailScreen extends StatefulWidget {
@@ -18,9 +16,12 @@ class MovieDetailScreen extends StatefulWidget {
 
 class _MovieDetailScreenState extends State<MovieDetailScreen> {
   late Future<MovieDetail> _futureMovieDetail;
+  int _rating = 0;
+
   @override
   void initState() {
     super.initState();
+    _loadRating();
     _futureMovieDetail = MovieDetailScreen.movieDb.getMovieDetail(widget.movieId);
 
   }
@@ -50,6 +51,41 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
       );
     }
   }
+
+  Widget _buildRatingStars() {
+    return Row(
+      children: [
+        for (int i = 1; i <= 5; i++)
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                _rating = i;
+                _saveRating(); // Salva a avaliação
+              });
+            },
+            child: Icon(
+              i <= _rating ? Icons.star : Icons.star_border,
+              color: Colors.yellow,
+              size: 40,
+            ),
+          ),
+      ],
+    );
+  }
+
+  void _loadRating() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int? savedRating = prefs.getInt('movie_${widget.movieId}_rating');
+    setState(() {
+      _rating = savedRating ?? 0;
+    });
+  }
+
+  void _saveRating() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('movie_${widget.movieId}_rating', _rating);
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -90,14 +126,29 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
                           movieDetail.overview,
                           style: TextStyle(fontSize: 16),
                         ),
-                        ElevatedButton(
-                          onPressed: () {
-                            addToWishlist(context, movieDetail);
-                          },
-                          child: Text('Adicionar à Lista de Desejos'),
-                        )
+                        Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center, // Centraliza os elementos horizontalmente
+                            children: [
+                              // Text('Sua avaliação: '),
+                              SizedBox(width: 8.0),
+                              _buildRatingStars(),
+                            ],
+                          ),
+                        ),
 
-
+                        Center(
+                          child: Column(
+                            children: [
+                              ElevatedButton(
+                                onPressed: () {
+                                  addToWishlist(context, movieDetail);
+                                },
+                                child: Text('Adicionar à Lista de Desejos'),
+                              ),
+                            ],
+                          ),
+                        ),
                       ],
                     ),
                   ),
